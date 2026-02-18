b34eb550f8ecaf82ee7ebdcd3e27d55c625d74c0
mana815 <237846458+mana815@users.noreply.github.com>
2026-02-18 20:18:50 +0100

    Add files via upload

---
+++ TAREA/vib/src/main/java/com/mycompany/vib/Vib.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vib;
+
+public class Vib {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
